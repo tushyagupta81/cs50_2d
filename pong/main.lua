73c228d7221d6174ffe9b06614b05aed3f8fe4a1
Tushya Gupta <tushyagupta81@gmail.com>
2026-04-26 17:09:03 +0530

    randomly moving ball

--- pong/main.lua
+++ pong/main.lua
@@ -4,20 +4,46 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_HEIGHT = 20
+
+PADDLE_SPEED = 200
+
 push = require("push")
 
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
 
-  smallFont = love.graphics.newFont("font.ttf", 8)
-  scoreFont = love.graphics.newFont("font.ttf", 32)
+	smallFont = love.graphics.newFont("font.ttf", 8)
+	scoreFont = love.graphics.newFont("font.ttf", 32)
 
-  love.graphics.setFont(smallFont)
+	math.random(os.time())
 
-  score = {
-    p1 = 0,
-    p2 = 0,
-  }
+	love.graphics.setFont(smallFont)
+
+	score = {
+		p1 = 0,
+		p2 = 0,
+	}
+
+	ball = {
+		x = VIRTUAL_WIDTH / 2 - 2,
+		y = VIRTUAL_HEIGHT / 2 - 2,
+	}
+
+	BALL_DX = math.random(2) == 1 and 100 or -100
+	BALL_DY = math.random(-50, 50)
+
+	paddle = {
+		left = 10,
+		right = VIRTUAL_HEIGHT - 30,
+	}
+
+	serve = {
+		p1 = true,
+		p2 = false,
+	}
+
+	gameState = "start"
 
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
 		resizable = false,
@@ -31,21 +57,33 @@ end
 function love.draw()
 	push:start()
 
-  love.graphics.clear(40/255, 45/255, 20/255, 255/255)
+	love.graphics.clear(40 / 255, 45 / 255, 20 / 255, 255 / 255)
 
-  love.graphics.setFont(smallFont)
-	love.graphics.printf("Hello, pong!", 0, 20, VIRTUAL_WIDTH, "center")
+	love.graphics.setFont(smallFont)
+	if gameState == "start" then
+		love.graphics.printf("Press enter to start", 0, 20, VIRTUAL_WIDTH, "center")
+	elseif gameState == "serve" then
+		if serve.p1 then
+			love.graphics.printf("Player 1 to serve, press enter", 0, 20, VIRTUAL_WIDTH, "center")
+		else
+			love.graphics.printf("Player 2 to serve, press enter", 0, 20, VIRTUAL_WIDTH, "center")
+		end
+	end
 
-  love.graphics.setFont(scoreFont)
+	love.graphics.setFont(scoreFont)
 	love.graphics.print(tostring(score.p1), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
 	love.graphics.print(tostring(score.p2), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-  love.graphics.setFont(smallFont)
-  love.graphics.rectangle("fill", 10, 10, 5, 20)
+	love.graphics.setFont(smallFont)
 
-  love.graphics.rectangle("fill", VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+	-- Paddle 1 LEFT
+	love.graphics.rectangle("fill", 10, paddle.left, 5, PADDLE_HEIGHT)
 
-  love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+	-- Paddle 2 RIGHT
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 15, paddle.right, 5, PADDLE_HEIGHT)
+
+	-- BALL
+	love.graphics.rectangle("fill", ball.x, ball.y, 4, 4)
 
 	push:finish()
 end
@@ -53,5 +91,36 @@ end
 function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
+	end
+	if key == "enter" or key == "return" then
+		if gameState == "start" then
+			gameState = "serve"
+		else
+			gameState = "play"
+			ball.x = VIRTUAL_WIDTH / 2 - 2
+			ball.y = VIRTUAL_HEIGHT / 2 - 2
+			BALL_DX = math.random(2) == 1 and 100 or -100
+			BALL_DY = math.random(-50, 50) * 1.5
+		end
+	end
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("up") then
+		paddle.right = math.max(0, paddle.right - PADDLE_SPEED * dt)
+	end
+	if love.keyboard.isDown("down") then
+		paddle.right = math.min(VIRTUAL_HEIGHT - PADDLE_HEIGHT, paddle.right + PADDLE_SPEED * dt)
+	end
+	if love.keyboard.isDown("w") then
+		paddle.left = math.max(0, paddle.left - PADDLE_SPEED * dt)
+	end
+	if love.keyboard.isDown("s") then
+		paddle.left = math.min(VIRTUAL_HEIGHT - PADDLE_HEIGHT, paddle.left + PADDLE_SPEED * dt)
+	end
+
+	if gameState == "play" then
+		ball.x = ball.x + BALL_DX * dt
+		ball.y = ball.y + BALL_DY * dt
 	end
 end
